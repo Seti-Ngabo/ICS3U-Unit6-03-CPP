ede8b7095ded94ac6b716ecf9d40679e862882a4
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-11-01 02:54:05 +0000

    added file

--- smallest_number.cpp
+++ smallest_number.cpp
@@ -9,44 +9,43 @@
 #include <array>
 
 template<size_t N>
-int lowestNumber(std::array<int, N> randoms) {
+int lowestNumber(std::array<int, N> numbers) {
     // this function creates random numbers
-    int maxNumber = 50;
+    int smallest_number = numbers[0];
 
     // process
-    for (int firstEachLoop : randoms) {
-        if (firstEachLoop < maxNumber) {
-            maxNumber = firstEachLoop;
+    for (int firstEachLoop : numbers) {
+        if (firstEachLoop < smallest_number) {
+            smallest_number = firstEachLoop;
         }
     }
 
-    return maxNumber;
+    return smallest_number;
 }
 
 int main() {
     // This function prints random numbers
-    std::array<int, 10> Numbers;
-
-    int eachLoop;
+    std::array<int, 10> numbers;
 
     int randomNumbers;
-    int finalLowest;
+    int smallest_number;
 
-    for (eachLoop = 0; eachLoop < 10; eachLoop++) {
-        std::random_device rseed;
-        std::mt19937 rgen(rseed());
-        std::uniform_int_distribution<int> idist(0, 50);
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution<int> idist(1, 100);
+
+    for (int eachLoop = 0; eachLoop < 10; eachLoop++) {
         randomNumbers = idist(rgen);
-        Numbers[eachLoop] = randomNumbers;
+        numbers[eachLoop] = randomNumbers;
         std::cout << "The random number " << eachLoop + 1 << " is: "
         << randomNumbers << std::endl;
     }
 
     // call functions
-    finalLowest = lowestNumber(Numbers);
+    smallest_number = lowestNumber(numbers);
 
     std::cout << "" << std::endl;
-    std::cout << "The smallest number is: " << finalLowest << std::endl;
+    std::cout << "The smallest number is: " << smallest_number << std::endl;
 
     std::cout << "\nDone." << std::endl;
 }
